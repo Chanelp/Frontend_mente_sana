5a490c8826d65c02bd9c027c8c2087d3b73d39b7
chanelp <mariannissanchez27@gmail.com>
2023-11-23 21:22:35 -0400

    Estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AboutUsComponent
+    }
+];
