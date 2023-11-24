2ec3ae44b07694faace4f440e0bdc92935afaf6d
chanelp <mariannissanchez27@gmail.com>
2023-11-23 23:30:53 -0400

    Páginas: about us & login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: AboutUsComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
