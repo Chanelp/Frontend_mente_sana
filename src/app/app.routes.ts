ab13508f312651e4eac43b5ec04855e8bd5b92d1
chanelp <mariannissanchez27@gmail.com>
2023-11-24 20:36:48 -0400

    Página: welcome y modificaciones a register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
     }
 ];
