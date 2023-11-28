818ea306a2a6d821a2044df3c5cb77a9ef65f548
Raynier Perez Minyety <rayniercraft@gmail.com>
2023-11-28 19:23:18 -0400

    css changes and not-found page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { Component } from '@angular/core';
+import { ProfesionalesComponent } from './pages/profesionales/profesionales.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +23,15 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
-    }
+    },
+    createRoute('professionals', ProfesionalesComponent),
+    createRoute('**', NotFoundComponent)
+
 ];
+
+function createRoute(path:string , component:any) {
+    return {
+        path, 
+        component
+    }
+}; 
