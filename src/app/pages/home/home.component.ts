fbeba177e06ccd252c9560566842fc8f2b2f50e3
chanelp <mariannissanchez27@gmail.com>
2023-12-11 13:51:51 -0400

    Fecha actual

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,64 +10,79 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  providers: [{ provide: JWT_OPTIONS, useValue: JWT_OPTIONS },JwtHelperService],
+  providers: [{ provide: JWT_OPTIONS, useValue: JWT_OPTIONS }, JwtHelperService],
   imports: [
     CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
   users = signal<User[]>([]);
   token: string;
   userId: number | string;
   userData: any;
-  
+
   private userService = inject(UserService);
   private router = inject(Router);
 
-  constructor(private jwtService: JwtHelperService){
+  constructor(private jwtService: JwtHelperService) {
     this.token = JSON.stringify(localStorage.getItem("token"));
   }
-  
-  ngOnInit(){
+
+  ngOnInit() {
+    this.getTodayDate();
     const decodedToken = this.jwtService.decodeToken(this.token);
     this.userId = decodedToken.sub
     console.log("EL ID USER ES", this.userId);
     this.getUserProfile();
   }
 
-  private getUsers(){
+  private getUsers() {
     this.userService.getAllUsers()
-    .subscribe({
-      next: (users) => {
-        console.log(users);
-        
-        this.users.set(users);
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
+      .subscribe({
+        next: (users) => {
+          console.log(users);
+
+          this.users.set(users);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
   }
 
-  logout(){
+  logout() {
     console.log("Cerrar sesion");
     localStorage.removeItem("token");
     this.router.navigateByUrl("/login");
   }
-  
-  getUserProfile(){
+
+  getUserProfile() {
     this.userService.getUserProfile(this.userId)
-    .subscribe({
-      next: (data) => {
-        this.userData = data;
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
+      .subscribe({
+        next: (data) => {
+          this.userData = data;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
   }
-  
+
+  getTodayDate() {
+    const today = new Date();
+
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const second = today.getSeconds();
+
+    const formattedDate = `${hour}:${minute} PM Today ${month}/${day}/${year}`;
+    return formattedDate;
+  }
+
 }
