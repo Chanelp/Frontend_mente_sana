27cdafd837df54c78c6b683c868b8494655ed81c
Adrian Vargas <110785300+ElLord0214@users.noreply.github.com>
2023-12-15 03:08:02 -0400

    hola

--- src/app/services/psicologos.service.ts
+++ src/app/services/psicologos.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+// psicologos.service.ts
+export interface Psicologo {
+  id: number;
+  nombre: string;
+  profesion: string;
+  descripcion: string;
+}
 
-@Injectable({
-  providedIn: 'root'
-})
 export class PsicologosService {
-
   constructor() { }
+
+  obtenerPsicologos(): Psicologo[] {
+    return [
+      { id: 1, nombre: 'Juan Pérez', profesion: 'Psicólogo Clínico', descripcion: 'Descripción de Juan Pérez...' },
+      { id: 2, nombre: 'María Rodríguez', profesion: 'Psicóloga Escolar', descripcion: 'Descripción de María Rodríguez...' },
+      // ...
+    ];
+  }
 }
