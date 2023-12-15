59eb18bfac11b78d3a9a8a6fcea6cfa58f8ae309
chanelp <mariannissanchez27@gmail.com>
2023-12-15 14:44:01 -0400

    Ajuste de estilos a user settings

--- src/app/pages/user-settings/user-settings.component.ts
+++ src/app/pages/user-settings/user-settings.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from '../../components/layout/layout.component';
+import { SidemenuComponent } from '../../components/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutComponent, SidemenuComponent],
   templateUrl: './user-settings.component.html',
   styleUrl: './user-settings.component.css'
 })
