acd878d5b863a6bf0cf6e83ac1a26e5a754a9019
tarea3-ionic <crismerydisla0550@gmail.com>
2023-12-14 20:46:52 -0400

    Cambios en registros, home y login

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistrarService } from '../../service_registrar/registrar.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -9,5 +12,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private registrarService: RegistrarService, private router: Router) { }
 
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    const formData = {
+      name: (event.target as HTMLFormElement)['nombre'].value,
+      last_name: (event.target as HTMLFormElement)['apellido'].value,
+      email: (event.target as HTMLFormElement)['email'].value,
+      password: (event.target as HTMLFormElement)['pass'].value,
+      date_birth: (event.target as HTMLFormElement)['date'].value,
+      genre: 'Género',
+      rol: 'User'
+    };
+
+    console.log('Haciendo solicitud de registro...');
+
+    this.registrarService.registerUser(formData).subscribe(
+      (response) => {
+        console.log('Usuario registrado:', response);
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.error('Error al registrar usuario:', error);
+      }
+    );
+  }
 }
