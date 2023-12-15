a3ca231a67cd17a45a2d9959b2cd02ac21c8d737
chanelp <mariannissanchez27@gmail.com>
2023-12-15 14:00:41 -0400

    Componente de modal para descripcion de profesionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CitasComponent } from './pages/citas/citas.component';
 import {UserSettingsComponent} from './pages/user-settings/user-settings.component';
-import { ProfesionalesComponent } from './pages/profesionales/profesionales.component';
 import { PsicologosListaComponent } from './pages/lista-psicologos/lista-psicologos.component';
 
 export const routes: Routes = [
@@ -34,7 +33,7 @@ export const routes: Routes = [
         component: CitasComponent
     },
     {
-        path: 'lista',
+        path: 'profesionales',
         component: PsicologosListaComponent
     }
     
--- src/app/pages/lista-psicologos/lista-psicologos.component.ts
+++ src/app/pages/lista-psicologos/lista-psicologos.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 import { CardProfesionalComponent } from '../../components/card-profesional/card-profesional.component';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from '../../components/layout/layout.component';
+import { ModalProfesionalComponent } from '../../components/modal-profesional/modal-profesional.component';
 
 @Component({
  selector: 'app-lista-psicologos',
  templateUrl: './lista-psicologos.component.html',
  standalone: true,
- imports: [CommonModule, LayoutComponent, CardProfesionalComponent],
+ imports: [CommonModule, LayoutComponent, CardProfesionalComponent, ModalProfesionalComponent],
  styleUrls: ['./lista-psicologos.component.css']
 })
 export class PsicologosListaComponent {
